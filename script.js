6e3f4ef6f959f61ee60fdcbf775ff806a1c64146
Faaiq <66476388+verhaxity@users.noreply.github.com>
2024-02-09 23:33:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+function startLoader() {
+    let counterElement = document.querySelector(".count p");
+    let currentValue = 0;
+
+
+    function updateCounter() {
+    if (currentValue < 100) {
+    let increment = Math.floor(Math.random() * 10) + 1;
+    currentValue = Math.min(currentValue + increment, 100);
+    counterElement.textContent = currentValue;
+
+
+    let delay = Math.floor(Math.random()* 200) + 25;
+    setTimeout(updateCounter, delay);
+    }
+}
+
+updateCounter();
+}
+
+startLoader();
+gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.5 });
+
+let textWrapper = document.querySelector(".ml16");
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [-100, 0],
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: (el, i) => 30* i
+})
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [0, 100],
+        easing: "easeOutExpo",
+        duration: 3000,
+        delay: (el, i) => 2000 + 30 * i
+});
+
+gsap.to(".pre-loader", {
+    scale: 0.5,
+    ease: "power4.inOut",
+    duration: 2,
+    delay: 3
+})
+
+gsap.to(".loader", {
+    height: "0",
+    ease: "power4.inOut",
+    duration: 1.5,
+    delay: 3.75
+})
+
+gsap.to(".loader-bg", {
+    height: "0",
+    ease: "power4.inOut",
+    duration: 1.5,
+    delay: 4
+})
+
+gsap.from(".header h1", {
+    y: 200,
+    ease: "power4.inOut",
+    duration: 1.5,
+    delay: 3.75,
+    stagger: 0.05   
+})
+
+gsap.to(".img", {
+    clipPath: "polygon (0 0, 100% 0, 100% 100%, 0 100%)",
+    ease: "power4.inOut",
+    duration: 1.5,
+    delay: 4.5,
+    stagger: 0.25
+})
